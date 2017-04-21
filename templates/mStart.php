311071bbec454b955290feaceb9f9b9691bc5afd
Rot <thaodo0492@yahoo.com>
2017-04-21 16:03:59 +0200

    faq page

--- templates/mStart.php
+++ templates/mStart.php
@@ -6,10 +6,8 @@
 		<meta name="viewport"  content="width=device-width, initial-scale=1.0">
 		<meta name="google-signin-client_id" content="887416469017-5bhes9v5h5isjimm79gdjck7dsnnc53s.apps.googleusercontent.com">
         <link href="../style.css" rel="stylesheet">
-        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
-        <link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css" />
-        <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
-        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
-	<link href="../styleLib.css" rel="stylesheet"> 
+        <link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.min.css" />
+        <link rel="stylesheet" type="text/css" href="../font-awesome/css/font-awesome.min.css" />
+	   <link href="../styleLib.css" rel="stylesheet"> 
 	</head>
 	<body>
