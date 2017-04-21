2df356c2abf3edc042a7dee9e52771c8af9f7f2f
Shawn <madsshawnhansen@hotmail.com>
2017-04-21 14:38:01 +0200

    added img styling

--- pages/profile.php
+++ pages/profile.php
@@ -5,7 +5,11 @@ include('../templates/header.php');
 ?>
 
 <div class="h-70">
-    
+    <div>
+            <div class="img-s-150">
+                        <img src="../images/anonymousProfile" class="img-100" alt="Profile">
+            </div>
+    </div>
 </div>
 
 <?php
