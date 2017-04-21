4649c2aef0296477f6d76dc356466165a3310ab1
Shawn <madsshawnhansen@hotmail.com>
2017-04-21 15:55:18 +0200

    added content

--- pages/profile.php
+++ pages/profile.php
@@ -38,6 +38,19 @@ include('../templates/header.php');
             </div>
     </div>
     
+    <hr class="hr-90">
+    
+            <div class="f-cl-c m-20">
+                <h1 class="h1-c">HISTORY</h2>
+                <div class="h-200 f-c m-20">
+                            <div class="img-s-150 m-l-r-50">
+                                        <img src="../images/genericItem.png" class="img-100" alt="Profile">
+                                        
+                            </div>
+                            <p class="w-300">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris maximus faucibus purus vel tempor. Nam mollis ipsum risus, eget rhoncus justo euismod et. Ut ut enim nisi. Aenean in pretium turpis, non maximus orci.</p>
+                </div>
+            </div>
+    
     
 </div>
 
