f47830f441a9dbacac9a39980e4fcb355b503b4b
Shawn <madsshawnhansen@hotmail.com>
2017-04-21 15:40:36 +0200

    Added content

--- pages/profile.php
+++ pages/profile.php
@@ -8,12 +8,14 @@ include('../templates/header.php');
     <div class="f-c m-20">
             <div class="f-sa w-250">
                         <div class="f-w-600">
+                                    <p class="">Name</p>
                                     <p class="">City</p>
                                     <p class="">Age</p>
                                     <p class="">...</p>
                                     <p class="">...</p>
                         </div>
                         <div>
+                                    <p class="">Thao</p>
                                     <p class="">Copenhagen</p>
                                     <p class="">24</p>
                                     <p class="">...</p>
@@ -27,6 +29,11 @@ include('../templates/header.php');
     <hr class="hr-90">
     <div class="f-c m-20">
             <h1 class="h1-c">CURRENT RENTALS</h2>
+            <div class="h-200">
+                        <div class="img-s-150 m-l-r-50">
+                                    <img src="../images/anonymousProfile.png" class="img-100" alt="Profile">      
+                        </div>
+            </div>
     </div>
     
     
