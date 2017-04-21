f139e9f4dcb5d728adec6f325f4e57372257d07c
Shawn <madsshawnhansen@hotmail.com>
2017-04-21 15:07:48 +0200

    Added stuff

--- pages/profile.php
+++ pages/profile.php
@@ -7,14 +7,21 @@ include('../templates/header.php');
 <div class="h-70">
     <div class="f-c m-20">
             <div>
-                        <p class="">Copenhagen</p>
-                        <p class="">24</p>
-                        <p class="">...</p>
-                        <p class="">...</p>
+                        <div>
+                                    <p class="">Copenhagen</p>
+                                    <p class="">24</p>
+                                    <p class="">...</p>
+                                    <p class="">...</p>
+                        </div>
+                        <div>
+                                    <p class="">Copenhagen</p>
+                                    <p class="">24</p>
+                                    <p class="">...</p>
+                                    <p class="">...</p>
+                        </div>
             </div>
             <div class="img-s-150 m-l-r-50">
-                        <img src="../images/anonymousProfile.png" class="img-100" alt="Profile">
-                                    
+                        <img src="../images/anonymousProfile.png" class="img-100" alt="Profile">      
             </div>
     </div>
     <hr class="hr-90">
