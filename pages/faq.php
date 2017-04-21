c0bb7f5d1d0fbb86e61448192fd68b3501e27f7d
Rot <thaodo0492@yahoo.com>
2017-04-21 13:22:07 +0200

    FAQ page

---
+++ pages/faq.php
@@ -0,0 +1,18 @@
+<?php
+
+    
+include('../templates/mStart.php');
+
+include('../templates/header.php');
+
+?>
+
+
+    
+ <?php  
+
+include('../templates/footer.php');
+
+include('../templates/mEnd.php');
+
+?>
