d358ae4ed5e0261b56d8e31a38e4b63181bf3116
Shawn <madsshawnhansen@hotmail.com>
2017-04-21 16:09:44 +0200

    Added stuff :dango:

--- pages/profile.php
+++ pages/profile.php
@@ -28,7 +28,7 @@ include('../templates/header.php');
     </div>
     <hr class="hr-90">
     <div class="f-cl-c m-20">
-            <h1 class="h1-c">CURRENT RENTALS</h2>
+            <h1 class="h1-c">CURRENT RENTALS</h1>
             <div class="h-200 f-c m-20">
                         <div class="img-s-150 m-l-r-50">
                                     <img src="../images/genericItem.png" class="img-100" alt="Profile">
@@ -41,14 +41,23 @@ include('../templates/header.php');
     <hr class="hr-90">
     
             <div class="f-cl-c m-20">
-                <h1 class="h1-c">HISTORY</h2>
-                <div class="h-200 f-c m-20">
-                            <div class="img-s-150 m-l-r-50">
-                                        <img src="../images/genericItem.png" class="img-100" alt="Profile">
-                                        
-                            </div>
-                            <p class="w-300">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris maximus faucibus purus vel tempor. Nam mollis ipsum risus, eget rhoncus justo euismod et. Ut ut enim nisi. Aenean in pretium turpis, non maximus orci.</p>
-                </div>
+                <h1 class="h1-c">HISTORY</h1>
+                <div class="b-2 m-30">
+                        <div>
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                        <i class="fa fa-star-o" aria-hidden="true"></i>
+                        </div>
+                        <div class="h-200 f-c m-20">
+                                    <div class="img-s-150 m-l-r-50">
+                                                <img src="../images/genericItem.png" class="img-100" alt="Profile">
+                                                
+                                    </div>
+                                    <p class="w-300">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris maximus faucibus purus vel tempor. Nam mollis ipsum risus, eget rhoncus justo euismod et. Ut ut enim nisi. Aenean in pretium turpis, non maximus orci.</p>
+                        </div>
+                    </div>
             </div>
     
     
