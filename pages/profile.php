e595b4dcb45e3429b379940aebd5ff673f138575
Shawn <madsshawnhansen@hotmail.com>
2017-04-21 15:53:09 +0200

    added image and content to profile.php

--- pages/profile.php
+++ pages/profile.php
@@ -31,7 +31,7 @@ include('../templates/header.php');
             <h1 class="h1-c">CURRENT RENTALS</h2>
             <div class="h-200 f-c m-20">
                         <div class="img-s-150 m-l-r-50">
-                                    <img src="../images/anonymousProfile.png" class="img-100" alt="Profile">
+                                    <img src="../images/genericItem.png" class="img-100" alt="Profile">
                                     
                         </div>
                         <p class="w-300">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris maximus faucibus purus vel tempor. Nam mollis ipsum risus, eget rhoncus justo euismod et. Ut ut enim nisi. Aenean in pretium turpis, non maximus orci.</p>
