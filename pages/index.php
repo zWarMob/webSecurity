9805287a167666b541e69bb46d10bac0d0bcd176
zWarMob <petyozh@yahoo.com>
2017-05-01 15:20:28 +0200

    lineHeight fix - products

--- pages/index.php
+++ pages/index.php
@@ -12,11 +12,11 @@ include('../templates/header.php');
 		<div class="filter b-2 p-10 w-200 fl-l m-r-20">
 			<h2 class="m-0 m-t-10">Date from</h2>
 			<ul class="p-0 m-0 ul-noDeco">
-				<li><input class="second-child" type="date" name="to"></li>
+				<li><input class="" type="date" name="to"></li>
 			</ul>
 			<h2 class="m-0 m-t-10">Date to</h2>
 			<ul class="p-0 m-0 ul-noDeco">
-				<li><input class="second-child" type="date" name="to"></li>
+				<li><input class="" type="date" name="to"></li>
 			</ul>
 			<h2 class="m-0 m-t-10">Categories</h2>
 			<ul class="p-0 m-0 ul-noDeco">
